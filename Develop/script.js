6be49684dbd2346727b4d079adbb7975341a393b
dorsariahi <dorsariahiasl@gmail.com>
2022-10-17 12:56:58 -0500

    added the past,present,and future id to html & js

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
-var currentDay=document.getElementById("currentDay")
-var currentDate=moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var currentDay = document.getElementById("currentDay")
+var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 currentDay.innerHTML = currentDate
+var textArea = $(".description")
+var currentHour = moment().hour()
+for(var i=0; i<textArea.length; i++){
+    var time = $(textArea[i]).parent().attr("id")
+if(time<currentHour){
+    $(textArea[i]).addClass("past")
+}
+if(time>currentHour){
+    $(textArea[i]).addClass("future")
+}
+if(time==currentHour){
+    $(textArea[i]).addClass("present")
+}
+}

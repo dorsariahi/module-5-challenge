a51c946d288c1613ba1d43e3a56c61842926855c
dorsariahi <dorsariahiasl@gmail.com>
2022-10-17 19:08:04 -0500

    formated the doc

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,15 @@ var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 currentDay.innerHTML = currentDate
 var textArea = $(".description")
 var currentHour = moment().hour()
-for(var i=0; i<textArea.length; i++){
+for (var i = 0; i < textArea.length; i++) {
     var time = $(textArea[i]).parent().attr("id")
-if(time<currentHour){
-    $(textArea[i]).addClass("past")
-}
-if(time>currentHour){
-    $(textArea[i]).addClass("future")
-}
-if(time==currentHour){
-    $(textArea[i]).addClass("present")
-}
+    if (time < currentHour) {
+        $(textArea[i]).addClass("past")
+    }
+    if (time > currentHour) {
+        $(textArea[i]).addClass("future")
+    }
+    if (time == currentHour) {
+        $(textArea[i]).addClass("present")
+    }
 }
